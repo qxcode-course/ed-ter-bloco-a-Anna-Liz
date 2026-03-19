343bfefc9ebc063b4178716f1e87bf042313819e
Anna-Liz <lizverissimo53@gmail.com>
2026-03-19 15:44:38 -0300

    pedra na lua

--- ed/pedra/go/draft.go
+++ ed/pedra/go/draft.go
@@ -1,5 +1,35 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
+
 func main() {
-    fmt.Println("qxcode")
+	var n int
+	fmt.Scan(&n)
+	var ganhador int
+	var aux int = 100
+
+	for i := range n {
+		var A, B int
+		fmt.Scanln(&A, &B)
+		var diferenca = A - B
+
+		if A > 9 && B > 9 {
+			if diferenca < 0 {
+				diferenca = -diferenca
+			}
+
+			if diferenca < aux {
+				aux = diferenca
+				ganhador = i
+			}
+		}
+	}
+
+	if ganhador == 0 {
+		fmt.Println("sem ganhador")
+	} else {
+		fmt.Println(ganhador)
+	}
 }
