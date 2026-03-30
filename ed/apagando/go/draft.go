a6b0e5c9ad9be75866f2dab60782a7986d1cef7e
Anna-Liz <lizverissimo53@gmail.com>
2026-03-30 17:55:55 -0300

    princesa e apagando

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -1,4 +1,37 @@
 package main
+
 import "fmt"
+
 func main() {
+	var n, m int
+	fmt.Scan(&n)
+
+	var fila []int
+	for range n {
+		var x int
+		fmt.Scan(&x)
+		fila = append(fila, x)
+	}
+
+	fmt.Scan(&m)
+	desistentes := make(map[int]struct{})
+	for range m {
+		var x int
+		fmt.Scan(&x)
+		desistentes[x] = struct{}{}
+	}
+
+	var fila_final []int
+	for i := 0; i < n; i++ {
+		_, existe := desistentes[fila[i]]
+		if existe == false {
+			fila_final = append(fila_final, fila[i])
+		}
+
+	}
+
+	for j := 0; j < len(fila_final); j++ {
+		fmt.Print(fila_final[j], " ")
+	}
+	fmt.Println()
 }
--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -1,5 +1,40 @@
 package main
+
 import "fmt"
+
+func printFila(fila []int, pos int) {
+	fmt.Print("[ ")
+	for i := 0; i < len(fila); i++ {
+		if i == pos {
+			fmt.Printf("%d> ", fila[i])
+		} else {
+			fmt.Printf("%d ", fila[i])
+		}
+	}
+	fmt.Println("]")
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	var n, e int
+	fmt.Scan(&n, &e)
+
+	var fila []int
+
+	for i := 1; i <= n; i++ {
+		fila = append(fila, i)
+	}
+
+	pos := e - 1
+	for len(fila) > 1 {
+		printFila(fila, pos)
+
+		morte := (pos + 1) % len(fila)
+
+		fila = append(fila[:morte], fila[morte+1:]...)
+
+		pos = morte % len(fila)
+	}
+
+	printFila(fila, pos)
+
 }
