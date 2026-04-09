9bafc895dc6fcf91c15328fd7046ab920da0904d
Anna-Liz <lizverissimo53@gmail.com>
2026-04-08 22:44:49 -0300

    resto eu acho

--- ed/resto/src/go/draft.go
+++ ed/resto/src/go/draft.go
@@ -1,5 +1,19 @@
 package main
+
 import "fmt"
+
+func resto(numero int) {
+	if numero == 0 {
+		return
+	}
+	resti := numero % 2
+	numero = numero / 2
+	resto(numero)
+	fmt.Println(numero, resti)
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	var x int = 0
+	fmt.Scan(&x)
+	resto(x)
 }
